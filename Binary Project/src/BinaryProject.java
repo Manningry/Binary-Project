3f6b512121c6f83997ef1d5d2885d76d971d0fbd
Ryan.Manning <Ryan.Manning@MU-C3-02.mullen.pvt.k12.co.us>
2014-12-16 08:57:26 -0700

    commit 2

--- Binary Project/src/BinaryProject.java
+++ Binary Project/src/BinaryProject.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 
 public class BinaryProject
 	{
-	static Scanner userInput = new Scanner(System.in);
+	
 	static ArrayList <Integer> numberLine = new ArrayList<Integer>();
+	static boolean notNegativeOne = true;
+	static Scanner userInput = new Scanner(System.in);
 	
 	public static void main(String[] args)
 		{
@@ -18,27 +20,37 @@ public class BinaryProject
 	public static void createNumLine()
 	{
 		System.out.println("Welcome to the binary sorter.\n");
-		System.out.println("First we need to make a list of numbers! Input as many numbers (1-20) as you would like, one at a time, pressing "
+		System.out.println("Input as many numbers (1-20) as you would like, one at a time, pressing "
 				+ "\nenter after each number. Please enter '-1' when you're finished!\n");
 		
-		while(true)
+		
+		while(notNegativeOne)
 			{ 
 				
 				int nextNumber = userInput.nextInt();
-				if(nextNumber >-1 && nextNumber <21)
+				
+				
+//				if(!userInput.hasNextInt())
+//					{
+//					System.out.println("Please try again.");
+//					boolean num = new Scanner(System.in).hasNextInt();
+//					}
+				if(nextNumber >-1 && nextNumber <21 && nextNumber > 0)
 				{
 					numberLine.add(nextNumber);
 				}
-				if(nextNumber <-1 || nextNumber > 20)
-				{
+				if(nextNumber <-1 || nextNumber > 20 || nextNumber == 0)
+					{
 					System.out.println("Please try again.");
-				}
-					
+					}
 				if(nextNumber == -1)
 				{
 					break;
 				}
 			}
+			
+		
+		
 	System.out.println(" ");
 	Collections.sort(numberLine);
 	for(int i = 0; i < numberLine.size(); i++)
@@ -51,11 +63,32 @@ public class BinaryProject
 	
 public static void testProgram()
 {
-int randomNum = (int) Math.random()* numberLine.size();
+int randomNum = (int) (Math.random()* numberLine.size());
+int target = numberLine.get(randomNum);
 
-System.out.println("\n\nYou are binary searching the following number:" + numberLine.get(randomNum));
+System.out.println("\n\nYour target number is:  " + target);
 System.out.println("\n\nPlease enter correct answers to the following questions about binary search tracing:");
 System.out.println("\n\nPass#   Left   Right   Middle   numberLine[middle]");	
+
+
+int left = 0; 
+int right = numberLine.size()-1;
+
+while(left < right)
+	{
+	int middle = (left+right)/2;
+	if(target < numberLine.get(middle))
+		{
+		right = middle -1;
+		}
+	if(target > numberLine.get(middle))
+		{
+		left = middle +1;
+		}
+	}
+
+
+
 	
 	
 }
